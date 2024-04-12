12d5859871fd8e2e5e84d706adb1d24492d9f3b5
Takuya Sudo <someone.said.so@gmail.com>
2024-04-12 23:49:10 +0900

    サーバー側にfirebase admin sdkの環境を構築

--- grpc/src/main.go
+++ grpc/src/main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"context"
+	firebase "firebase.google.com/go/v4"
+	"log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// https://firebase.google.com/docs/admin/setup?hl=ja#go
+
+	// Initialize default app
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	// Access auth service from the default app
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	auth := r.Header.Get("Authorization")
 	fmt.Println("Authorization: ", auth)
